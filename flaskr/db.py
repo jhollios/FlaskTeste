2a18aed52231f48c21ef113eabd020c59d2c71f3
igorsilvademorais <igorsilva.morais@ee.ufcg.edu.br>
2022-12-14 17:52:18 -0300

    Initial Commit

---
+++ flaskr/db.py
@@ -0,0 +1,10 @@
+import sqlite3
+import click
+from flask import current_app, g
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config[]
+        )
