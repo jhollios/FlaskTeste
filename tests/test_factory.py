f61799d31956da2609858c36c6c0f83dd011c273
igorsilvademorais <igorsilva.morais@ee.ufcg.edu.br>
2022-12-21 17:10:53 -0300

    Quarto commit

---
+++ tests/test_factory.py
@@ -0,0 +1,11 @@
+from flaskr import create_app
+
+
+def test_config():
+    assert not create_app().testing
+    assert create_app({'TESTING': True}).testing
+
+
+def test_hello(client):
+    response = client.get('/hello')
+    assert response.data == b'Ol\xc3\xa1, mundo!'
